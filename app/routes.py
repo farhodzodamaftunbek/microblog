489d25d254a4d8e2d67c47200f46d180e2eefc29
farhodzodamaftunbek <farhodzodamaftunbek8@gmail.com>
2026-04-21 08:29:24 +0500

    chapter 5 done

---
+++ app/routes.py
@@ -0,0 +1,54 @@
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user,logout_user,login_required
+import sqlalchemy as sa
+from app import db
+from app.forms import RegistrationForm
+from app.models import User
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = {'username': 'Otabek'}
+    posts = [
+        {'author': {'username': 'Abdulaziz'}, 'body': 'Beautiful day in Dushanbe!'},
+        {'author': {'username': 'Ahmadjon'}, 'body': 'The Invincible series was so cool!'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
